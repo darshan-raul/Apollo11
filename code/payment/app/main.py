aa5ff74b0b9d54b8592cb21cc201eecbf86d5fd8
Darshan Raul <dar.raul@gmail.com>
2024-10-07 19:11:43 +0000

    adds prometheus k9s manifests and tests with payment app

--- code/payment/app/main.py
+++ code/payment/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends,FastAPI,HTTPException
+from fastapi import Depends,FastAPI,HTTPException,Response
 from sqlmodel import select
 from sqlmodel import Session
 from sqlalchemy.exc import SQLAlchemyError
@@ -6,7 +6,12 @@ from app.db import get_session, init_db
 
 from app.models import Payment,PaymentCreate
 
+from prometheus_client import Counter, Gauge, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+
 app = FastAPI()
+
+GET_REQUESTS = Counter('get_requests_total', 'Total number of GET requests')
 
 
 @app.on_event("startup")
@@ -16,6 +21,7 @@ def on_startup():
 
 @app.get("/started")
 def check_db_readiness(session: Session = Depends(get_session)):
+    GET_REQUESTS.inc()
     try:
         # Perform a simple query
         session.exec(select(Payment).limit(1)).first()
@@ -23,17 +29,23 @@ def check_db_readiness(session: Session = Depends(get_session)):
     except SQLAlchemyError as e:
         raise HTTPException(status_code=503, detail=f"Database is not ready: {str(e)}")
 
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
 
 @app.get("/ready")
 async def ready():
+    GET_REQUESTS.inc()
     return {"yesiam": "ready!"}
 
 @app.get("/ping")
 async def pong():
+    GET_REQUESTS.inc()
     return {"ping": "pong!"}
 
 @app.get("/payments", response_model=list[Payment])
 def get_payments(session: Session = Depends(get_session)):
+    GET_REQUESTS.inc()
     result = session.execute(select(Payment))
     payments = result.scalars().all()
     return [Payment(mode=payment.mode, price=payment.price, id=payment.id, status=payment.status) for payment in payments]
