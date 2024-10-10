2fa9671b9a1087906f1283f491d95def75767829
Darshan Raul <dar.raul@gmail.com>
2024-10-10 13:55:33 +0000

    adds initial metrics to movie service

--- Tiltfile
+++ Tiltfile
@@ -1,4 +1,5 @@
 docker_build("payment", "code/payment")
+docker_build("movie", "code/movie")
+
 k8s_yaml(['stages/stage-5/payment/app/payment-configmap.yaml','stages/stage-5/payment/app/payment-secret.yaml','stages/stage-5/payment/app/payment-service.yaml','stages/stage-5/payment/app/payment-deployment.yaml','stages/stage-5/payment/db/payment-db-data-persistentvolumeclaim.yaml', 'stages/stage-5/payment/db/payment-db-secret.yaml','stages/stage-5/payment/db/payment-db-service.yaml','stages/stage-5/payment/db/payment-db-statefulset.yaml'])
-
-
+k8s_yaml(['stages/stage-5/movie/app/movie-configmap.yaml','stages/stage-5/movie/app/movie-secret.yaml','stages/stage-5/movie/app/movie-service.yaml','stages/stage-5/movie/app/movie-deployment.yaml','stages/stage-5/movie/db/movie-db-data-persistentvolumeclaim.yaml', 'stages/stage-5/movie/db/movie-db-secret.yaml','stages/stage-5/movie/db/movie-db-service.yaml','stages/stage-5/movie/db/movie-db-statefulset.yaml'])
--- code/movie/app/main.py
+++ code/movie/app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Response
 from pymongo import MongoClient
 from pydantic import BaseModel
 import httpx
 import os
+
+from prometheus_client import Counter, Gauge, Histogram, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+import time 
 
 class Movie(BaseModel):
     name: str
@@ -19,6 +23,15 @@ theatrePort = os.environ['THEATRE_PORT']
 
 app = FastAPI()
 
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint', 'status'])
+MOVIE_COUNT = Gauge('movies_count', 'Number of movies in the database')
+REQUEST_LATENCY = Histogram(
+    'request_latency_seconds', 
+    'Latency of requests in seconds', 
+    ['method', 'endpoint']
+)
+
+MOVIES_CREATED = Counter('movies_created', 'Total movies added to the db')
 
 def get_database():
  
@@ -31,21 +44,42 @@ def get_database():
 
 db = get_database()
 
+@app.middleware("http")
+async def measure_latency(request, call_next):
+    method = request.method
+    endpoint = request.url.path
+    start_time = time.time()
+    
+    response = await call_next(request)
+    request_duration = time.time() - start_time
+    REQUEST_LATENCY.labels(method, endpoint).observe(request_duration) 
+    return response
+
+@app.get("/metrics")
+def metrics():
+    REQUESTS.labels(method='GET', endpoint='/metrics', status='2xx').inc()
+    return Response(generate_latest(), media_type="text/plain")
+
+
 @app.get("/ping")
 async def root():
+    REQUESTS.labels(method='GET', endpoint='/ping', status='2xx').inc()
     return {"message": "pong"}
 
 @app.get("/ready")
 async def root():
+    REQUESTS.labels(method='GET', endpoint='/ready', status='2xx').inc()
     return {"message": "ready"}
 
 @app.get("/started")
 async def root():
+    REQUESTS.labels(method='GET', endpoint='/started', status='2xx').inc()
     return {"message": "started"}
 
 @app.get("/movies")
 async def movies():
-    collection_name = db["user_1_items"]
+    REQUESTS.labels(method='GET', endpoint='/movies', status='2xx').inc()
+    collection_name = db["movies"]
     result = collection_name.find()
     movies = []
     for doc in result:
@@ -55,18 +89,21 @@ async def movies():
         movie["theatres"] = doc["theatres"]
         movie["genre"] = doc["genre"]
         movies.append(movie)
+    MOVIE_COUNT.set(len(movies))
     return movies
 
 @app.get("/movies/{movie}")
 async def getmovie(movie):
-    collection_name = db["user_1_items"]
+    REQUESTS.labels(method='GET', endpoint='/movies/<movie>', status='2xx').inc()
+    collection_name = db["movies"]
     result = collection_name.find_one({"movie_name": movie})
     return {"message": result}
 
 
 @app.post("/movies")
 async def create_movie(movie: Movie):
-    collection_name = db["user_1_items"]
+    
+    collection_name = db["movies"]
    
     theatresrequest = httpx.get(f'http://{theatreUrl}:{theatrePort}/theatres')
     theatreslist = theatresrequest.json()
@@ -83,4 +120,6 @@ async def create_movie(movie: Movie):
     "theatres": theatres
     }
     collection_name.insert_one(moviejson)
+    MOVIES_CREATED.inc()
+    REQUESTS.labels(method='POST', endpoint='/movies', status='2xx').inc()
     return {"message": movie}
